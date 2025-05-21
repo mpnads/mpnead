8fc30f09566ff15c35377bd47020296ff44f42ba
mpnads <mpnads@gmail.com>
2025-05-21 10:38:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+<script>
+            document.querySelectorAll('.navbar-collapse .nav-link').forEach(function (navLink) {
+                navLink.addEventListener('click', function () {
+                    const navbarCollapse = document.querySelector('.navbar-collapse');
+                    if (navbarCollapse.classList.contains('show')) {
+                        new bootstrap.Collapse(navbarCollapse).toggle();
+                    }
+                });
+            });
+             document.querySelectorAll('.footer-link, .nav-link').forEach(anchor => {
+                anchor.addEventListener('click', function (e) {
+                    if (this.getAttribute('href') === '#home') {
+                        e.preventDefault();
+                        window.scrollTo({
+                            top: 0,
+                            behavior: 'smooth'
+                        });
+                    } else if (this.hash !== '') {
+                        e.preventDefault();
+                        const target = document.querySelector(this.hash);
+                        if (target) {
+                            target.scrollIntoView({
+                                behavior: 'smooth',
+                                block: 'start'
+                            });
+                        }
+                    }
+                });
+            });
+         </script>
+         <!-- Date & Time Script -->
+        <script>
+            function updateDateTime() {
+                const now = new Date();
+                const date = now.toLocaleDateString();
+                const time = now.toLocaleTimeString();
+                document.getElementById('date-time').textContent = `${date} ${time}`;
+            }
+            setInterval(updateDateTime, 1000);
+            updateDateTime(); // initial call
+        </script>
+        <!-- Date & Time Script End -->
+         <script>
+            function loadContent(url) {
+                const contentDiv = document.getElementById('dynamic-content');
+                if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+                    // If it's an image
+                    contentDiv.innerHTML = `<img src="${url}" class="img-fluid" alt="Loaded Content">`;
+                } else {
+                    // If it's another HTML page or link
+                    contentDiv.innerHTML = `<iframe src="${url}" width="100%" height="600px" frameborder="0"></iframe>`;
+                }
+            }
+         </script>
